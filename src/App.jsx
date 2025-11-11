6f6b0eebc7f7d0a2486f9f52dfe3461561da0578
TechnoJavBar <javibarrancajz@gmail.com>
2025-11-11 22:16:40 +0100

    se ha añadido la ruta para la lista de proveedores

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-import './App.css'
-import {Routes, Route} from 'react-router-dom'
-import Home from './components/home/Home.jsx'
-import Layout from './components/Layout.jsx'
-import Header from './components/header/Header.jsx'
-import Register from './components/register/Register.jsx'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home.jsx";
+import Layout from "./components/Layout.jsx";
+import Header from "./components/header/Header.jsx";
+import Register from "./components/register/Register.jsx";
+import Login from "./components/login/Login.jsx";
+import { ProveedoresList } from "./components/proveedores/ProveedoresList.jsx";
 
 function App() {
-
-  return(
-    <div className='app'>
-      <Header/>
+  return (
+    <div className="app">
+      <Header />
       <Routes>
-        <Route path='/' element={<Layout/>}>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/register' element={<Register/>}/>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/proveedores" element={<ProveedoresList />} />
         </Route>
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
