02b60afb01d2cb871bd12efb3987c45434b95b4c
TechnoJavBar <javibarrancajz@gmail.com>
2025-11-11 20:19:37 +0100

    Se ha cambiado la tematica remplazando usuarios por productos

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,10 +6,10 @@ import ToastContainer from "react-bootstrap/ToastContainer";
 import Toast from "react-bootstrap/Toast";
 
 const Home = () => {
-  const url_get = "http://localhost:4000/usuarios";
-  const url_delete = "http://localhost:4000/usuario/delete";
+  const url_get = "http://localhost:4000/productos";
+  const url_delete = "http://localhost:4000/productos/delete";
 
-  const [user, setUser] = useState([]);
+  const [producto, setProducto] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [message, setMessage] = useState("");
@@ -17,45 +17,51 @@ const Home = () => {
   const [isErrorDelete, setIsErrorDelete] = useState(false);
 
   useEffect(() => {
-    const fetchUsers = async () => {
+    const fetchProductos = async () => {
       try {
         const response = await fetch(url_get);
 
         if (!response.ok) {
-          throw new Error(`Error HTTP: ${response.status} ${response.statusText}`);
+          throw new Error(
+            `Error HTTP: ${response.status} ${response.statusText}`
+          );
         }
 
         const data = await response.json();
-        setUser(data);
+        setProducto(data);
       } catch (err) {
-        console.error("Fallo al cargar los usuarios:", err);
+        console.error("Fallo al cargar los productos:", err);
         setError(err.message);
       } finally {
         setLoading(false);
       }
     };
 
-    fetchUsers();
+    fetchProductos();
   }, []);
 
-  const handleDelete = async (user) => {
-    console.log("Eliminando usuario:", user.username);
-  
+  const handleDelete = async (producto) => {
+    console.log("Eliminando producto:", producto.idProducto);
+
     try {
-      const response = await fetch(`${url_delete}/${user.username}`, {
+      const response = await fetch(`${url_delete}/${producto.idProducto}`, {
         method: "DELETE",
       });
-  
+
       const res = await response.json();
       console.log(res);
-  
+
       if (!response.ok) {
-        setMessage(`Error al eliminar al usuario ${res.error || res.statusText}`);
+        setMessage(
+          `Error al eliminar el producto ${res.error || res.statusText}`
+        );
         setIsErrorDelete(true);
         setShowToast(true);
       } else {
-        setMessage("El usuario se ha eliminado correctamente");
-        setUser((prevUsers) => prevUsers.filter((u) => u.username !== user.username));
+        setMessage("El producto se ha eliminado correctamente");
+        setProducto((prevProducto) =>
+          prevProducto.filter((u) => u.idProducto !== producto.idProducto)
+        );
         setShowToast(true);
       }
     } catch (err) {
@@ -65,10 +71,9 @@ const Home = () => {
       setShowToast(true);
     }
   };
-  
 
   if (loading) {
-    return <p>Cargando usuarios desde la base de datos...</p>;
+    return <p>Cargando productos desde la base de datos...</p>;
   }
 
   if (error) {
@@ -76,47 +81,60 @@ const Home = () => {
       <div style={{ color: "red" }} className="container">
         <p>Error al conectar con la API:</p>
         <p>Detalle: {error}</p>
-        <p>Asegúrate de que tu servidor Node.js esté corriendo en el puerto 4000.</p>
+        <p>
+          Asegúrate de que tu servidor Node.js esté corriendo en el puerto 4000.
+        </p>
       </div>
     );
   }
 
-  if (user.length === 0) {
-    return <p>No se encontraron usuarios en la base de datos</p>;
+  if (producto.length === 0) {
+    return <p>No se encontraron productos en la base de datos</p>;
   }
 
   return (
     <div className="container">
-      <h2>Lista de usuarios Neo4j</h2>
-      {user.map((user, index) => (
+      <h2>Lista de productos Neo4j</h2>
+      {producto.map((producto, index) => (
         <Card border="info" key={index} style={{ margin: "5px" }}>
-          <Card.Header>{user.name}</Card.Header>
+          <Card.Header>{producto.nombre}</Card.Header>
           <Card.Body>
-            <Card.Title>Username: {user.username}</Card.Title>
-            <Card.Text>Password: {user.password}</Card.Text>
-            <Button variant="danger" onClick={() => handleDelete(user)}>
+            <Card.Title>identificador: {producto.idProducto}</Card.Title>
+            <Card.Text>categoría: {producto.categoria}</Card.Text>
+            <Card.Text>stock: {producto.stock} uds</Card.Text>
+            <Button variant="danger" onClick={() => handleDelete(producto)}>
               Eliminar
             </Button>
           </Card.Body>
         </Card>
       ))}
-  
+
       {/*Se encarga de mostrar el mensaje ya sea de error o de satisfactorio*/}
-        <ToastContainer
-            className='p-3'
-            position='bottom-end'
-            style={{zIndex: 1}}
+      <ToastContainer
+        className="p-3"
+        position="bottom-end"
+        style={{ zIndex: 1 }}
+      >
+        <Toast
+          onClose={() => setShowToast(false)}
+          show={showToast}
+          delay={3000}
+          autohide
         >
-        <Toast onClose={() => setShowToast(false)} show={showToast} delay={3000} autohide>
-            <Toast.Header>
-                <strong variant="me-auto">{isErrorDelete ? 'error':'satisfacftorio'}</strong>
-                <small>11 mins ago</small>
-            </Toast.Header>
-            <Toast.Body style={{color: "black"}} className={isErrorDelete ? 'Danger':'Success'}>
-                {message}
-            </Toast.Body>
+          <Toast.Header>
+            <strong variant="me-auto">
+              {isErrorDelete ? "error" : "satisfacftorio"}
+            </strong>
+            <small>11 mins ago</small>
+          </Toast.Header>
+          <Toast.Body
+            style={{ color: "black" }}
+            className={isErrorDelete ? "Danger" : "Success"}
+          >
+            {message}
+          </Toast.Body>
         </Toast>
-        </ToastContainer>
+      </ToastContainer>
     </div>
   );
 };
