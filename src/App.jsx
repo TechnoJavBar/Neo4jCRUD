22e2a62b18947e8cf6ef1eb46badf1823dd7d27e
TechnoJavBar <javibarrancajz@gmail.com>
2025-11-12 11:43:10 +0100

    Se ha añadido la ruta para la lista de productos

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Header from "./components/header/Header.jsx";
 import Register from "./components/register/Register.jsx";
 import Login from "./components/login/Login.jsx";
 import { ProveedoresList } from "./components/proveedores/ProveedoresList.jsx";
+import { ProductosList } from "./components/productos/ProductosList.jsx";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/productos" element={<ProductosList />} />
           <Route path="/proveedores" element={<ProveedoresList />} />
         </Route>
       </Routes>
