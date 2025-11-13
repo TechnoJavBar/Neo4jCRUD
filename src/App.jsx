d866d50c6fe6b94685b6a785285a1471e1ee3531
TechnoJavBar <javibarrancajz@gmail.com>
2025-11-13 20:27:15 +0100

    Se ha creado la ruta para la creacion de proveedores

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Register from "./components/register/Register.jsx";
 import Login from "./components/login/Login.jsx";
 import { ProveedoresList } from "./components/proveedores/ProveedoresList.jsx";
 import { ProductosList } from "./components/productos/ProductosList.jsx";
+import { RegisterProveedor } from "./components/register/RegisterProveedor.jsx";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/register/proveedor" element={<RegisterProveedor />} />
           <Route path="/login" element={<Login />} />
           <Route path="/productos" element={<ProductosList />} />
           <Route path="/proveedores" element={<ProveedoresList />} />
